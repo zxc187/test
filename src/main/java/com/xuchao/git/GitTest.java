8bc94e5c5e91b911b4df481400a982abbe7b4fe7
zxc <z1871001618@163.com>
2023-06-26 11:01:50 +0800

    222

---
+++ src/main/java/com/xuchao/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xuchao.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+
+
+    }
+}
